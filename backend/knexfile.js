b770c95d1503c9c9be4bda5d04e7de1f71649da9
kellraym <ray.al.kell@gmail.com>
2021-07-14 22:27:28 -0600

    Still trying to get docker to work

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,10 +1,10 @@
-// Update with your config settings.
+const connectionString = process.env.DB_CONNECTION_STRING
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: 'postgres://postgres:docker@localhost/crud_app_raykell'
+    connection: connectionString
   },
 
   staging: {
