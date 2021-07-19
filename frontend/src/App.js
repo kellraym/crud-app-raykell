40c06b5dc360cf312245034550e835b37efce807
kellraym <ray.al.kell@gmail.com>
2021-07-19 14:08:19 -0600

    try without App.listen()

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,9 +5,9 @@ import Home from './Components/Home'
 import AddItem from './Components/AddItem'
 import DisplayCompleted from './Components/DisplayCompleted';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-const port = process.env.port || 3000;
+// const port = process.env.port || 3000;
 
-App.listen(port)
+// App.listen(port)
 
 function App() {
   document.title = 'To Do List 2.0'
@@ -20,7 +20,7 @@ function App() {
           <section>
             <Route exact path="/" component={Home} />
             <Route path="/additem" component={AddItem} />
-            <Route path ="/complete" component={DisplayCompleted} />
+            <Route path="/complete" component={DisplayCompleted} />
           </section>
         </div>
       </Switch>
