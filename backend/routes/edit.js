44737600313942d4e6fdfadf42501db30c960d27
kellraym <ray.al.kell@gmail.com>
2021-07-19 12:47:52 -0600

    change knex routes to production

--- backend/routes/edit.js
+++ backend/routes/edit.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const knex = require('knex')(require('../knexfile.js')['development']);
+const knex = require('knex')(require('../knexfile.js')['production']);
 
 /* GET users listing. */
-router.get('/edit/:id', function(req, res, next) {
+router.get('/edit/:id', function (req, res, next) {
   res.send('respond with a resource');
 });
 
