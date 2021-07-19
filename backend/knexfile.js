d231a393415cf261801f0f1279bf25cd9fdc38b6
kellraym <ray.al.kell@gmail.com>
2021-07-19 12:20:53 -0600

    try database url in production

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,5 +1,5 @@
 const connectionString = process.env.DB_CONNECTION_STRING;
-const otherConnectionString = 'postgres://xkscfwhexwkvfz:b2ea5d82a0716785e24c07af9f7ac175ffe3594d72d3386a38277280a491f684@ec2-52-72-125-94.compute-1.amazonaws.com:5432/deai91bpi9bc0s'
+const databaseUrl = process.env.DATABASE_URL
 module.exports = {
 
   development: {
@@ -26,13 +26,13 @@ module.exports = {
   production: {
     client: 'pg',
     connection: {
-      otherConnectionString,
+      databaseUrl,
       ssl: {
         rejectUnauthorized: false,
       },
-      database: 'deai91bpi9bc0s',
-      user: 'xkscfwhexwkvfz',
-      password: 'b2ea5d82a0716785e24c07af9f7ac175ffe3594d72d3386a38277280a491f684'
+      // database: 'deai91bpi9bc0s',
+      // user: 'xkscfwhexwkvfz',
+      // password: 'b2ea5d82a0716785e24c07af9f7ac175ffe3594d72d3386a38277280a491f684'
     },
     pool: {
       min: 2,
